8738f9be46c24f63c4167b16f221d7c7ca7e5c0a
Dickson Tam <dicksont650@gmail.com>
2015-06-13 11:33:57 -0700

    Updated tests to use array-etc.

--- spec/simple.js
+++ spec/simple.js
@@ -42,29 +42,8 @@
   var document = window.document;
   var propene = window.propene;
 
-  Array.prototype.equals = Array.prototype.equals || function (array) {
-    // if the other array is a falsy value, return
-    if (!array)
-      return false;
-
-    // compare lengths - can save a lot of time
-    if (this.length != array.length)
-      return false;
-
-    for (var i = 0, l=this.length; i < l; i++) {
-      // Check if we have nested arrays
-      if (this[i] instanceof Array && array[i] instanceof Array) {
-        // recurse into the nested arrays
-        if (!this[i].equals(array[i]))
-          return false;
-        }
-        else if (this[i] != array[i]) {
-          // Warning - two different object instances will never be equal: {x:20} != {x:20}
-          return false;
-        }
-    }
-    return true;
-  }
+  var arrload = require('array-etc');
+  arrload(['equals']);
 
   QUnit.test('propene', function(assert) {
     var ui = {};
